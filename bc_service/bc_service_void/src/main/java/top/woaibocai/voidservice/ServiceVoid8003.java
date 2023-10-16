10ba56b9404af00842023f0a1e8f339a8e802e27
woaibocai <365191806@qq.com>
2023-10-16 23:54:47 +0800

    腾讯云点播

--- bc_service/bc_service_void/src/main/java/top/woaibocai/voidservice/ServiceVoid8003.java
+++ bc_service/bc_service_void/src/main/java/top/woaibocai/voidservice/ServiceVoid8003.java
@@ -1,4 +1,4 @@
-package top.woaibocai;
+package top.woaibocai.voidservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
